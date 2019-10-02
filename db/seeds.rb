a73df1712f8f2a299d5fff7ad890430881daea7c
Jon Tran <trungt8998@gmail.com>
2019-10-02 17:04:29 -0500

    Add faker gem and edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Faker::Config.locale = 'en-CA'
+
+Customer.destroy_all
+Checkin.destroy_all
+Technician.destroy_all
+Service.destroy_all
+
+8.times do 
+  Customer.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    birth_date: Faker::Date.birthday(min_age: 6, max_age: 80),
+    phone_num: Faker::PhoneNumber.unique.cell_phone
+  )
+end
