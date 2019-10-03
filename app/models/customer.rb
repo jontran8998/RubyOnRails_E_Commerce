8e1322b08b6f495139b3e7840d0fb33a3cac9177
Jon Tran <trungt8998@gmail.com>
2019-10-02 19:59:57 -0500

    Create inter-model Customer_Checkin

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 class Customer < ApplicationRecord
   has_many :checkins
-
+  has_many :customer_checkins
+  has_many :checkins, through: :customer_checkins
   validates :first_name, :last_name, :email, :birth_date, :phone_num, presence: true
   validates :phone_num, numericality: { only_integer: true }
 end
