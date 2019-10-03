cbe5b34dae17eb24a9e4b65019ca78ec80c18886
Jon Tran <trungt8998@gmail.com>
2019-10-03 12:47:47 -0500

    Add show single customer route and controller

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -2,4 +2,8 @@ class CustomerController < ApplicationController
   def index
     @customers = Customer.all
   end
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'checkins', to: 'checkin#index'
   get 'customers', to: 'customer#index'
+  get 'customers/:id', to: 'customer#show'
   get 'about', to: 'about#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
