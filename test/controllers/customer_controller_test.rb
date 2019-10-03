0e9f61141cacea9113a7559231cde0b6786eee66
Jon Tran <trungt8998@gmail.com>
2019-10-03 08:36:12 -0500

    Create customer controller and set up route

---
+++ test/controllers/customer_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CustomerControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get customer_index_url
+    assert_response :success
+  end
+
+end
