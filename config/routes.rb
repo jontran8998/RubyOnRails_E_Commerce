2540e9e5487f666409153e9d10227303fc7c21d3
Jon Tran <trungt8998@gmail.com>
2019-10-03 22:46:05 -0500

    Fix Route on customer and checkin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  get 'checkins', to: 'checkin#index'
-  get 'checkins/:id', to: 'checkin#show'
-  get 'customers', to: 'customer#index'
-  get 'customers/:id', to: 'customer#show'
-  get 'about', to: 'about#index'
+  
+  resources :checkin, only: %i[index show]
+  # get 'checkins', to: 'checkin#index'
+  # get 'checkins/:id', to: 'checkin#show'
+
+  resources :customer, only: %i[index show]
+  # get 'customers', to: 'customer#index'
+  # get 'customers/:id', to: 'customer#show'
+
+  resources :about, only: :index
+
+  # get 'about', to: 'about#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'checkin#index'
 end
